9bbc2c157a16872e0fcad12c2c90ddef95169ac6
Ashish Khanchandani <akhanchandani1996@gmail.com>
2020-01-13 00:28:47 -0500

    initial commit

---
+++ IntroProject/IntroProject.py
@@ -0,0 +1,26 @@
+
+#Intro print statements
+print("Hello World!") 
+print("Will is Awesome!")
+
+#Practice assigning variables
+ashish = 23 # integer
+ashish2= "Hello World 2!" # string
+ashish3 = False #true or false
+
+#Print Variables
+print(ashish)
+print(ashish2)
+print(ashish3)
+print(type(ashish3))
+print(type(ashish))
+print(type(ashish2))
+
+#doing some math
+math1 = ashish + 24
+print(math1)
+
+#string math
+string2 = "This world is Awesome!"
+strcombo = ashish2 + string2
+print(strcombo)
